9897b2fd27e8a4668009c7cac06c198e094f0d1e
Simon Delineau <simondelineau@gmail.com>
2019-05-20 23:39:52 +0200

    Update MainThread.java

--- src/main/MainThread.java
+++ src/main/MainThread.java
@@ -6,44 +6,44 @@ import GUI.GamePanel;
 import misc.Const;
 
 public class MainThread extends Thread {
-    private GamePanel gp;
+	private GamePanel gp;
 
-    public MainThread(GamePanel gp) throws IOException {
-        super();
-        this.gp = gp;
-    }
+	public MainThread(GamePanel gp) throws IOException {
+		super();
+		this.gp = gp;
+	}
 
-    @Override
-    public void run() {
-        long beginTime;
-        int sleepTime = 0;
-        int framesSkipped;
-        long timeDiff;
-        while (true) {
-            try {
-                beginTime = System.currentTimeMillis();
-                framesSkipped = 0;
-                gp.update();
-                gp.repaint();
-                timeDiff = System.currentTimeMillis() - beginTime;
-                sleepTime = (int) (Const.FRAME_PERIOD - timeDiff);
-                if (sleepTime > 0) {
-                    try {
-                        Thread.sleep(sleepTime);
-                    } catch (InterruptedException e) {
-                    }
-                }
-                while (sleepTime < 0 && framesSkipped < Const.MAX_FRAM_SKIPS) {
-                    gp.update();
-                    sleepTime += Const.FRAME_PERIOD;
-                    framesSkipped++;
-                }
-            } finally {
-            }
-        }
-    };
+	@Override
+	public void run() {
+		long beginTime;
+		int sleepTime = 0;
+		int framesSkipped;
+		long timeDiff;
+		while (true) {
+			try {
+				beginTime = System.currentTimeMillis();
+				framesSkipped = 0;
+				gp.update();
+				gp.repaint();
+				timeDiff = System.currentTimeMillis() - beginTime;
+				sleepTime = (int) (Const.FRAME_PERIOD - timeDiff);
+				if (sleepTime > 0) {
+					try {
+						Thread.sleep(sleepTime);
+					} catch (InterruptedException e) {
+					}
+				}
+				while (sleepTime < 0 && framesSkipped < Const.MAX_FRAM_SKIPS) {
+					gp.update();
+					sleepTime += Const.FRAME_PERIOD;
+					framesSkipped++;
+				}
+			} finally {
+			}
+		}
+	};
 
-    private void storeStats() {
+	private void storeStats() {
 
-    }
+	}
 }
