13a0754426c80328034c36b0079fca298d87b74d
Simon Delineau <simondelineau@gmail.com>
2019-05-20 23:12:38 +0200

    fix speed chost

--- src/GUI/GamePanel.java
+++ src/GUI/GamePanel.java
@@ -24,107 +24,104 @@ import service.ReadTxtService;
 
 public class GamePanel extends JPanel {
 
-    private static GamePanel instance;
+	private static GamePanel instance;
 
-    public static synchronized GamePanel getInstance() throws IOException {
-        if (instance == null)
-            instance = new GamePanel();
-        return instance;
-    }
+	public static synchronized GamePanel getInstance() throws IOException {
+		if (instance == null)
+			instance = new GamePanel();
+		return instance;
+	}
 
-    private MainThread t;
-    private GridService gs;
-    private ReadTxtService read;
-    private AbstractPersonnage pacMan;
-    private FieldService fs;
-    private LinkService ls;
+	private MainThread t;
+	private GridService gs;
+	private ReadTxtService read;
+	private AbstractPersonnage pacMan;
+	private FieldService fs;
+	private LinkService ls;
 
-    private GhostService ghs;
-    private long fps;
+	private GhostService ghs;
+	private long fps;
 
-    private List<AbstractPersonnage> personnages = new ArrayList<>();
+	private List<AbstractPersonnage> personnages = new ArrayList<>();
 
-    private GamePanel() throws IOException {
-        super();
-        gs = GridService.getInstance();
-        fs = FieldService.getInstance();
-        ls = LinkService.getInstance();
-        read = ReadTxtService.getInstance(new File("terrain.txt"));
-        Map<Integer, List<String>> data = read.readFile();
-        ghs = GhostService.getInstance();
+	private GamePanel() throws IOException {
+		super();
+		gs = GridService.getInstance();
+		fs = FieldService.getInstance();
+		ls = LinkService.getInstance();
+		read = ReadTxtService.getInstance(new File("terrain.txt"));
+		Map<Integer, List<String>> data = read.readFile();
+		ghs = GhostService.getInstance();
 
-        fs.createField(data);
-        ls.craeteLink();
+		fs.createField(data);
+		ls.craeteLink();
 
-        Dimension spawnPM = new Dimension(9, 11);
-        Dimension spawnG = new Dimension(9, 9);
+		Dimension spawnPM = new Dimension(9, 11);
+		Dimension spawnG = new Dimension(9, 9);
 
-        pacMan = new PacMan(gs.getPixelFromPosition(spawnPM).width, gs.getPixelFromPosition(spawnPM).height,
-                (Const.SIZE_F.width / Const.NBR_COL), (Const.SIZE_F.height / Const.NBR_ROW), 6);
+		pacMan = new PacMan(gs.getPixelFromPosition(spawnPM).width, gs.getPixelFromPosition(spawnPM).height,
+				(Const.SIZE_F.width / Const.NBR_COL), (Const.SIZE_F.height / Const.NBR_ROW), 6);
 
-        Ghost ghost1 = new Ghost(gs.getPixelFromPosition(spawnG).width, gs.getPixelFromPosition(spawnG).height,
-                (Const.SIZE_F.width / Const.NBR_COL), (Const.SIZE_F.height / Const.NBR_ROW));
+		Ghost ghost1 = new Ghost(gs.getPixelFromPosition(spawnG).width, gs.getPixelFromPosition(spawnG).height,
+				(Const.SIZE_F.width / Const.NBR_COL), (Const.SIZE_F.height / Const.NBR_ROW));
 
-        personnages.add(pacMan);
-        personnages.add(ghost1);
+		personnages.add(pacMan);
+		personnages.add(ghost1);
 
-        // ls.printNodeAndLink();
+		// ls.printNodeAndLink();
 
-        this.setPreferredSize(Const.SIZE_F);
-        t = new MainThread(this);
-        t.start();
-    }
+		this.setPreferredSize(Const.SIZE_F);
+		t = new MainThread(this);
+		t.start();
+	}
 
-    public long getFps() {
-        return fps;
-    }
+	public long getFps() {
+		return fps;
+	}
 
-    public AbstractPersonnage getPacMan() {
-        return this.pacMan;
-    }
+	public AbstractPersonnage getPacMan() {
+		return this.pacMan;
+	}
 
-    public Thread getThread() {
-        return this.t;
-    }
+	public Thread getThread() {
+		return this.t;
+	}
 
-    @Override
-    public void paintComponent(Graphics g) {
-        this.reset(g);
+	@Override
+	public void paintComponent(Graphics g) {
+		this.reset(g);
 
-        for (Wall wall : Wall.getWalls())
-            wall.paint(g);
-        // for (AbstractNode node : AbstractNode.getNodes())
-        // node.paint(g);
-        for (AbstractPersonnage personnage : personnages)
-            personnage.paint(g);
+		for (Wall wall : Wall.getWalls())
+			wall.paint(g);
+		// for (AbstractNode node : AbstractNode.getNodes())
+		// node.paint(g);
+		for (AbstractPersonnage personnage : personnages)
+			personnage.paint(g);
 
-    }
+	}
 
-    private void reset(Graphics g) {
-        g.fillRect(0, 0, Const.SIZE_F.width, Const.SIZE_F.height);
+	private void reset(Graphics g) {
+		g.fillRect(0, 0, Const.SIZE_F.width, Const.SIZE_F.height);
 
-    }
+	}
 
-    public void setFps(long l) {
-        this.fps = l;
-    }
+	public void setFps(long l) {
+		this.fps = l;
+	}
 
-    public void setPacMan(AbstractPersonnage pacMan) {
-        this.pacMan = pacMan;
-    }
+	public void setPacMan(AbstractPersonnage pacMan) {
+		this.pacMan = pacMan;
+	}
 
-    public void setThread(MainThread t) {
-        this.t = t;
-    }
+	public void setThread(MainThread t) {
+		this.t = t;
+	}
 
-    public void update() {
-        System.out.println(fps);
-        for (AbstractPersonnage personnage : personnages) {
-            personnage.move();
-        }
-        for (Ghost ghost : Ghost.getGhosts()) {
-            ghost.move();
-        }
-    }
+	public void update() {
+		System.out.println(fps);
+		for (AbstractPersonnage personnage : personnages) {
+			personnage.move();
+		}
+	}
 
 }
