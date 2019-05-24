13b3c83417906602486dd69b13b5248fddd600d7
Simon Delineau <delineau@idmgroup.com>
2019-05-24 16:55:26 +0200

    fix ghost back issue

--- src/element/Ghost.java
+++ src/element/Ghost.java
@@ -12,96 +12,100 @@ import service.NodeService;
 
 public class Ghost extends AbstractPersonnage {
 
-	private static List<Ghost> ghosts;
+    private static List<Ghost> ghosts;
 
-	public static List<Ghost> getGhosts() {
-		return ghosts;
-	}
+    public static List<Ghost> getGhosts() {
+        return ghosts;
+    }
 
-	public static void setGhosts(List<Ghost> ghosts) {
-		Ghost.ghosts = ghosts;
-	}
+    public static void setGhosts(List<Ghost> ghosts) {
+        Ghost.ghosts = ghosts;
+    }
 
-	private GhostService ghs;
+    private GhostService ghs;
 
-	private NodeService ns;
+    private NodeService ns;
 
-	private List<AbstractNode> chemin;
+    private List<AbstractNode> chemin;
 
-	private AbstractNode currentNode;
+    private AbstractNode currentNode;
 
-	public Ghost(int x, int y, int width, int height, int gap) {
-		super(x, y, width, height, gap);
-		ns = NodeService.getInstance();
-		ghs = GhostService.getInstance();
-		if (ghosts == null)
-			ghosts = new ArrayList<>();
-		ghosts.add(this);
-		chemin = new ArrayList<>();
-		currentNode = ns.getNodeByPos(this.actualPos);
-	}
+    public Ghost(int x, int y, int width, int height, int gap) {
+        super(x, y, width, height, gap);
+        ns = NodeService.getInstance();
+        ghs = GhostService.getInstance();
+        if (ghosts == null)
+            ghosts = new ArrayList<>();
+        ghosts.add(this);
+        chemin = new ArrayList<>();
+        currentNode = ns.getNodeByPos(this.actualPos);
+        speed = 150;
+    }
 
-	private void findDirection() {
-		if (!this.chemin.isEmpty()) {
-			AbstractNode target = this.chemin.get(0);
-			if (!target.getPos().equals(this.actualPos)) {
-				if (target.getPos().height == this.actualPos.height) {
-					if (target.getPos().width > this.actualPos.width) {
-						resetDirection(false);
-						right = true;
-					} else if (target.getPos().width < this.actualPos.width) {
-						resetDirection(false);
-						left = true;
-					}
-				} else if (target.getPos().width == this.actualPos.width) {
-					if (target.getPos().height > this.actualPos.height) {
-						resetDirection(false);
-						down = true;
-					} else if (target.getPos().height < this.actualPos.height) {
-						resetDirection(false);
-						up = true;
-					}
-				}
-			} else {
-				this.currentNode = target;
-				resetDirection(false);
-				this.chemin.remove(0);
-			}
-		}
-	}
+    private void findDirection() {
+        if (!this.chemin.isEmpty()) {
+            AbstractNode target = this.chemin.get(0);
+            if (!target.getPos().equals(this.actualPos)) {
+                if (target.getPos().height == this.actualPos.height) {
+                    if (target.getPos().width > this.actualPos.width) {
+                        resetDirection(false);
+                        right = true;
+                        this.currentNode = target;
+                    } else if (target.getPos().width < this.actualPos.width) {
+                        resetDirection(false);
+                        left = true;
+                        this.currentNode = target;
+                    }
+                } else if (target.getPos().width == this.actualPos.width) {
+                    if (target.getPos().height > this.actualPos.height) {
+                        resetDirection(false);
+                        down = true;
+                        this.currentNode = target;
+                    } else if (target.getPos().height < this.actualPos.height) {
+                        resetDirection(false);
+                        up = true;
+                        this.currentNode = target;
+                    }
+                }
+            } else {
+                resetDirection(false);
+                this.chemin.remove(0);
+            }
+        }
+    }
 
-	public List<AbstractNode> getChemin() {
-		return this.chemin;
-	}
+    public List<AbstractNode> getChemin() {
+        return this.chemin;
+    }
 
-	public AbstractNode getCurrentNode() {
-		return this.currentNode;
-	}
+    public AbstractNode getCurrentNode() {
+        return this.currentNode;
+    }
 
-	@Override
-	public void move() {
-//		A AMELIORER
-		for (double i = 0; i < speed / Const.MAX_FPS; i++) {
-			this.actualPos = gs.getPositionFromPixel(new Dimension(this.anchor.width, this.anchor.height));
-			this.centered = gs.isCentered(this);
-			if (this.centered)
-				findDirection();
-			direction();
-		}
-	}
+    @Override
+    public void move() {
+        // A AMELIORER
+        for (double i = 0; i < speed / Const.MAX_FPS; i++) {
+            this.actualPos = gs.getPositionFromPixel(new Dimension(this.anchor.width, this.anchor.height));
+            this.centered = gs.isCentered(this);
+            if (this.centered)
+                findDirection();
+            direction();
+        }
+    }
 
-	@Override
-	public void paint(Graphics g) {
-		g.setColor(Color.PINK);
-		g.fillOval((int) this.x, (int) this.y, width, height);
-	}
+    @Override
+    public void paint(Graphics g) {
+        g.setColor(Color.PINK);
+        g.fillOval((int) this.x, (int) this.y, width, height);
+    }
 
-	public void setChemin(List<AbstractNode> chemin) {
-		this.chemin = chemin;
-	}
+    public void setChemin(List<AbstractNode> chemin) {
+        this.chemin = chemin;
+    }
 
-	public void setCurrentNode(AbstractNode currentNode) {
-		this.currentNode = currentNode;
-	}
+    public void setCurrentNode(AbstractNode currentNode) {
+        this.currentNode = currentNode;
+    }
 
 }
--- src/misc/Const.java
+++ src/misc/Const.java
@@ -3,11 +3,11 @@ package misc;
 import java.awt.Dimension;
 
 public class Const {
-	public static final Dimension SIZE_F = new Dimension(800, 800);
-	public static final int NBR_COL = 17;
-	public static final int NBR_ROW = 17;
-	public final static double MAX_FPS = 60;
-	public final static int MAX_FRAM_SKIPS = 5;
-	public final static double FRAME_PERIOD = 1000 / MAX_FPS;
-	public final static int NBR_GHOSTS = 1;
+    public static final Dimension SIZE_F = new Dimension(800, 800);
+    public static final int NBR_COL = 17;
+    public static final int NBR_ROW = 17;
+    public final static double MAX_FPS = 60;
+    public final static int MAX_FRAM_SKIPS = 5;
+    public final static double FRAME_PERIOD = 1000 / MAX_FPS;
+    public final static int NBR_GHOSTS = 3;
 }
